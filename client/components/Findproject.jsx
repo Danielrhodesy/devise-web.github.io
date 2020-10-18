6fd4c528d8af2b36fe8be6a193279fd2f519d9ed
Daniel Rhodes <danielrhodesnz@gmail.com>
2020-10-19 12:28:12 +1300

    Styling Homepage

--- client/components/Findproject.jsx
+++ client/components/Findproject.jsx
@@ -5,11 +5,16 @@ class Findproject extends Component {
   render() {
     return (
       <>
-        <section class="fp-container sa ct">
-            <article class="bl">
+        <section>
+          <div className="background-gif">
+            <img src="/public/dude.png" alt="Main Gif"/>
+          </div>
+        </section>
+        <section className="fp-container sa  ct">
+            <article className="bl ct fdv sa">
                 <h1>Find a Project</h1>
-                <div class="search"></div>
-                <div class="button">Search</div>   
+                <div className="search">I am a</div>
+                <div className="button">Search</div>   
             </article>
         </section>
       </>
