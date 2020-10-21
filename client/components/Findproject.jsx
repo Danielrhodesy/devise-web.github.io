ab441a7620c184d082874434a7f68ad87ead1210
Daniel Rhodes <danielrhodesnz@gmail.com>
2020-10-22 12:09:02 +1300

    Added drop down menu, need proper dropdown items and smoother styling

--- client/components/Findproject.jsx
+++ client/components/Findproject.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-
+import DropdownButton from 'react-bootstrap/DropdownButton'
+import Dropdown from 'react-bootstrap/Dropdown'
 
 
 class Findproject extends Component {
@@ -10,9 +11,19 @@ class Findproject extends Component {
         <section className="fp-container sa  ct">
             <article className="bl ct fdv sa">
                 <h1>Find a Project</h1>
-                <div className="search-container sb">
+                <div className="search-container sa">
                   <div className="search">I am a ..</div>
-                  <div className="search-drop"></div>
+                  <Dropdown>
+                    <Dropdown.Toggle variant="success" id="dropdown-basic" className="search-drop">
+                      Creative Process
+                    </Dropdown.Toggle>
+
+                    <Dropdown.Menu className="dropdown fdv">
+                      <Dropdown.Item href="#/action-1" className="dropdown-item ct sa">Action</Dropdown.Item>
+                      <Dropdown.Item href="#/action-2" className="dropdown-item ct sa">Another action</Dropdown.Item>
+                      <Dropdown.Item href="#/action-3" className="dropdown-item ct sa">Something else</Dropdown.Item>
+                    </Dropdown.Menu>
+                  </Dropdown>
                 </div>
                 <div className="button">Search</div>   
             </article>
--- client/components/Spotlight.jsx
+++ client/components/Spotlight.jsx
@@ -6,13 +6,20 @@ class Spotlight extends Component {
     return (
       <>
         <section class="sl-container sb">
-            <article class="sl-bio">
-                <h1>Spotlight</h1>
-                <p>Project of the month</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sed neque sit amet sapien dictum venenatis. Phasellus efficitur lacus et neque iaculis bibendum. Ut fermentum scelerisque lectus ac fringilla. Suspendisse. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sed neque sit amet sapien dictum venenatis. Phasellus efficitur lacus et neque iaculis bibendum. Ut fermentum scelerisque lectus ac fringilla. Suspendisse. </p>
+            <article class="sl-bio fdv ct">
+                <div className="bio-title"></div>
+                <h3>Project of the month</h3>
+                <div className="bio-text">
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sed neque sit amet sapien dictum venenatis. Phasellus efficitur lacus et neque iaculis bibendum. Ut fermentum scelerisque lectus ac fringilla. Suspendisse. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sed neque sit amet sapien dictum venenatis. Phasellus efficitur lacus et neque iaculis bibendum. Ut fermentum scelerisque lectus ac fringilla. Suspendisse. </p>
+                </div>
             </article>
-            <article class="sl-gallery">
-
+            <article className="sl-gallery sb">
+              <div className="img-container-left fdv">
+                <div className="img-cont bl"></div>
+                <div className="img-cont bl"></div>
+                <div className="img-cont bl"></div>
+              </div>
+              <div className="img-container-right bl"></div>
             </article>
         </section>
       </>
