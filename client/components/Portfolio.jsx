cbb8e75368744988ea701393efaeeec548e22fbc
Daniel Rhodes <danielrhodesnz@gmail.com>
2020-10-27 23:50:16 +1300

    Added all iimages, Devise logo not there, going to host

--- client/components/Portfolio.jsx
+++ client/components/Portfolio.jsx
@@ -1,15 +1,36 @@
 import React, { Component } from "react";
+import port1 from "../../public/port-images/portfolio.png"
+import port2 from "../../public/port-images/portfolio2.png"
+import port3 from "../../public/port-images/portfolio3.png"
+import port4 from "../../public/port-images/portfolio4.png"
+import port5 from "../../public/port-images/portfolio5.png"
+import port6 from "../../public/port-images/portfolio6.png"
+import port7 from "../../public/port-images/portfolio7.png"
+import port8 from "../../public/port-images/portfolio8.png"
+import port9 from "../../public/port-images/portfolio9.png"
+import port10 from "../../public/port-images/portfolio10.png"
+import port11 from "../../public/port-images/portfolio11.png"
 
 
 class Portfolio extends Component {
   render() {
     return (
       <>
-        <section class="pf-container">
+        <section id="photos" class="pf-container">
             <article class="pf-title-container">
-                <h1>Portfolio</h1>
             </article>
             <article class="pf-gallery">
+              <img src={port1} alt=""/>
+              <img src={port2} alt=""/>
+              <img src={port3} alt=""/>
+              <img src={port4} alt=""/>
+              <img src={port5} alt=""/>
+              <img src={port6} alt=""/>
+              <img src={port7} alt=""/>
+              <img src={port8} alt=""/>
+              <img src={port9} alt=""/>
+              <img src={port10} alt=""/>
+              <img src={port11} alt=""/>
 
             </article>
         </section>
